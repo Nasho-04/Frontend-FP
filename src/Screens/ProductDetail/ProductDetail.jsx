c97483497f81d6c200ac6d0216728ec274ef7088
Nacho <nachonieva04@gmail.com>
2024-12-11 18:47:53 -0300

    Changed some fonts and the whole ProductDetail screen, also fixed some image errors on the home

--- src/Screens/ProductDetail/ProductDetail.jsx
+++ src/Screens/ProductDetail/ProductDetail.jsx
@@ -16,7 +16,7 @@ const ProductDetail = () => {
     const [product, setProduct] = useState("")
     const { image, handleChangeFile, setImage, loading, setLoading } = useGlobalContext()
     const user_info = JSON.parse(sessionStorage.getItem('user_info'))
-    const authorized = product.seller_id === user_info.id  || user_info.role === 'admin'
+    const authorized = product.seller_id === user_info.id || user_info.role === 'admin'
     const [editMode, setEditMode] = useState(false)
     const [toggleAdd, setToggleAdd] = useState(false)
     const [toggleDelete, setToggleDelete] = useState(false)
@@ -97,91 +97,93 @@ const ProductDetail = () => {
 
     return (
         <>
-        <Navbar />
-        <LoadingOverlay />
-        <div className='product-detail-screen'>
-            <div className='product-detail-container'>
-                {product 
-                ?<>
-                <div className='product-detail-image-container'>
-                    <img src={product.image} alt={product.name} />
+            <Navbar />
+            <LoadingOverlay />
+            <div className='product-detail-screen'>
+                <div className='product-detail-container'>
+                    {product
+                        ? <>
+                            <div className='product-detail-image-container'>
+                                <img src={product.image} alt={product.name} />
+                            </div>
+                            <div className='product-detail-info-container'>
+                                <span>
+                                    <h1 className='product-detail-title'>{product.name}</h1>
+                                    <span className='product-detail-price'>ARS ${product.price}</span>
+                                    <p className='product-detail-stock'>Available stock: {product.stock}</p>
+                                    <p className='product-detail-description'>{product.description}</p>
+                                </span>
+                                <div className='product-detail-button-container'>
+                                    {!authorized && <button className='product-detail-button' onClick={() => setToggleAdd(true)}>Add to Cart</button>}
+                                    {authorized && <button className='product-detail-button edit-button' onClick={() => setEditMode(!editMode)}>Edit Product</button>}
+                                    {authorized && <button className='product-detail-button delete-button' onClick={() => setToggleDelete(true)}>Delete Product</button>}
+                                </div>
+                            </div>
+                        </>
+                        :
+                        <div className='loading-container'>
+                            <span className='loading-spinner'></span>
+                        </div>
+                    }
                 </div>
-                <div className='product-detail-info-container'>
-                    <h1 className='product-detail-title'>{product.name}</h1>
-                    <span className='product-detail-price'>${product.price}</span>
-                    <p className='product-detail-stock'>Available stock: {product.stock}</p>
-                    <p className='product-detail-description'>{product.description}</p>
-                    <div className='product-detail-button-container'>
-                        {!authorized && <button className='product-detail-button' onClick={() => setToggleAdd(true)}>Add to Cart</button>}
-                        {authorized && <button className='product-detail-button edit-button' onClick={() => setEditMode(!editMode)}>Edit Product</button>}
-                        {authorized && <button className='product-detail-button delete-button' onClick={() => setToggleDelete(true)}>Delete Product</button>}
-                    </div>
+                {/* FORM */}
+                <div className='product-detail-form-container' style={{ display: editMode ? 'flex' : 'none' }}>
+                    <h2 className='product-detail-form-title'>Update Your Product</h2>
+                    <span className='edit-product-error'></span>
+                    <form onSubmit={handleSubmitEditProductForm} action="" className='edit-product-form'>
+                        <div className='edit-product-left'>
+                            <div className='edit-product-field'>
+                                <label htmlFor="name">Name: </label>
+                                <input className='edit-product-input' type="text" id="name" name="name" required maxLength="25" />
+                            </div>
+                            <div className='edit-product-field'>
+                                <label htmlFor="price">Price: </label>
+                                <input className='edit-product-input' type="number" id="price" name="price" required min="0" />
+                            </div>
+                            <div className='edit-product-field'>
+                                <label htmlFor="category">Category: </label>
+                                <select name="category" id="category" required>
+                                    <option value="Electronics">Electronics</option>
+                                    <option value="Clothing">Clothing</option>
+                                    <option value="Home">Home</option>
+                                    <option value="Toys">Toys</option>
+                                    <option value="Books">Books</option>
+                                    <option value="Health">Health</option>
+                                    <option value="Beauty">Beauty</option>
+                                </select>
+                            </div>
+                            <div className='edit-button-container'>
+                                <button className='edit-product-button confirm' type="submit">Confirm Product</button>
+                                <div onClick={() => setEditMode(false)} type="reset" className='edit-product-button cancel'>Cancel</div>
+                            </div>
+                        </div>
+                        <div className='edit-product-right'>
+                            <div className='edit-product-field'>
+                                <label htmlFor="stock">Stock: </label>
+                                <input className='edit-product-input' type="number" id="stock" name="stock" required min="0" max="100" />
+                            </div>
+                            <div className='edit-product-field edit-image'>
+                                <label htmlFor="image">Image: </label>
+                                <input className='edit-product-image' type="file" id="image" name="image" onChange={handleChangeFile} required />
+                                {image
+                                    ?
+                                    <span><i className="bi bi-check2"></i></span>
+                                    : null
+                                }
+                            </div>
+                            <div className='edit-product-field'>
+                                <label htmlFor="description">Description: </label>
+                                <textarea rows="5" cols="50" maxLength={"255"} className='edit-product-textarea' type="text" id="description" name="description" required />
+                            </div>
+                        </div>
+                    </form>
                 </div>
-                </>
-                : 
-                    <div className='loading-container'>
-                        <span className='loading-spinner'></span>
-                    </div>
-                }
+                <Cart />
+                <Overlay toggle={toggleAdd} setToggle={setToggleAdd} product={product} btnFunction={addToCart} btnText1="Confirm" btnText2="Cancel" text="Are you sure you want to add this product?" />
+                <Overlay toggle={toggleDelete} setToggle={setToggleDelete} product={product} btnFunction={handleDeleteProduct} btnText1="Confirm" btnText2="Cancel" text="Are you sure you want to delete this product?" />
+                <Overlay toggle={deleteConfirm} setToggle={setDeleteConfirm} product={product} btnFunction={() => navigate(`/home`)} btnText1="Go Home" text="Product deleted successfully!" />
+                <Overlay toggle={editConfirm} setToggle={setEditConfirm} product={product} btnFunction={() => navigate(`/home`)} btnText1="Go Home" text="Product updated successfully!" />
             </div>
-            {/* FORM */}
-            <div className='product-detail-form-container' style={{ display: editMode ? 'flex' : 'none' }}>
-                <h2 className='product-detail-form-title'>Update Your Product</h2>
-                <span className='edit-product-error'></span>
-                <form onSubmit={handleSubmitEditProductForm} action="" className='edit-product-form'>
-                    <div className='edit-product-left'>
-                        <div className='edit-product-field'>
-                            <label htmlFor="name">Name: </label>
-                            <input className='edit-product-input' type="text" id="name" name="name" required maxLength="25" />
-                        </div>
-                        <div className='edit-product-field'>
-                            <label htmlFor="price">Price: </label>
-                            <input className='edit-product-input' type="number" id="price" name="price" required min="0" />
-                        </div>
-                        <div className='edit-product-field'>
-                            <label htmlFor="category">Category: </label>
-                            <select name="category" id="category" required>
-                                <option value="Electronics">Electronics</option>
-                                <option value="Clothing">Clothing</option>
-                                <option value="Home">Home</option>
-                                <option value="Toys">Toys</option>
-                                <option value="Books">Books</option>
-                                <option value="Health">Health</option>
-                                <option value="Beauty">Beauty</option>
-                            </select>
-                        </div>
-                        <div className='edit-button-container'>
-                            <button className='edit-product-button confirm' type="submit">Confirm Product</button>
-                            <div onClick={() => window.location.reload()} type="reset" className='edit-product-button cancel'>Reset</div>
-                        </div>
-                    </div>
-                    <div className='edit-product-right'>
-                        <div className='edit-product-field'>
-                            <label htmlFor="stock">Stock: </label>
-                            <input className='edit-product-input' type="number" id="stock" name="stock" required min="0" max="100" />
-                        </div>
-                        <div className='edit-product-field edit-image'>
-                            <label htmlFor="image">Image: </label>
-                            <input className='edit-product-image' type="file" id="image" name="image" onChange={handleChangeFile} required />
-                            {image
-                                ?
-                                <span><i className="bi bi-check2"></i></span>
-                                : null
-                            }
-                        </div>
-                        <div className='edit-product-field'>
-                            <label htmlFor="description">Description: </label>
-                            <textarea rows="5" cols="50" maxLength={"255"} className='edit-product-textarea' type="text" id="description" name="description" required />
-                        </div>
-                    </div>
-                </form>
-            </div>
-            <Cart />
-            <Overlay toggle={toggleAdd} setToggle={setToggleAdd} product={product} btnFunction={addToCart} btnText1="Confirm" btnText2="Cancel" text="Are you sure you want to add this product?" />
-            <Overlay toggle={toggleDelete} setToggle={setToggleDelete} product={product} btnFunction={handleDeleteProduct} btnText1="Confirm" btnText2="Cancel" text="Are you sure you want to delete this product?" />
-            <Overlay toggle={deleteConfirm} setToggle={setDeleteConfirm} product={product} btnFunction={() => navigate(`/home`)} btnText1="Go Home" text="Product deleted successfully!" />
-            <Overlay toggle={editConfirm} setToggle={setEditConfirm} product={product} btnFunction={() => navigate(`/home`)} btnText1="Go Home" text="Product updated successfully!" />
-        </div>
         </>
     )
 }
